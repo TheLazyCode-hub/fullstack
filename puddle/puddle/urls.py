7b211bf769f270a2caba3cdc38cb009a5bb7abee
TheLazyCode-hub <gauravshukla052@gmail.com>
2023-02-06 23:35:53 +0530

    conversation added one way

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('dashboard/', include('dashboard.urls')),
-    path('items/', include('item.urls'))
+    path('items/', include('item.urls')),
+    path('inbox/', include('conversation.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
