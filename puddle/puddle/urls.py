0c1ff5795f533c05044a93804684f6251bde28f3
TheLazyCode-hub <gauravshukla052@gmail.com>
2023-02-06 20:02:59 +0530

    dashboard added

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('items/', include('item.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
