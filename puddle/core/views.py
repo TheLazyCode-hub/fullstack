02476a80014609a99880303fe7ee902637d765d8
TheLazyCode-hub <gauravshukla052@gmail.com>
2023-02-07 00:04:07 +0530

    signout and some problems solved

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from item.models import Item, Category
 from .forms import SignupForm
+from django.contrib.auth import logout
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
@@ -27,3 +28,7 @@ def signup(request):
     return render(request, 'core/signup.html', {
         'form':form
     })
+
+def logout_request(request):
+    logout(request)
+    return redirect("/")
